74fadc7495fc38b3c4e0874bbb95b48c9b44f6fa
Aaron <J>
2015-08-04 07:44:27 -0700

    Got pin persistence working again.
    Now need to re-implement removing pins from the map.

--- Virtual Tourist/Pin.swift
+++ Virtual Tourist/Pin.swift
@@ -19,22 +19,40 @@ class Pin: NSManagedObject
     @NSManaged var pinNumber: Int16
     @NSManaged var mapPin: MKPinAnnotationView
     
-    init( location: CLLocationCoordinate2D, number: Int16, pin: MKPinAnnotationView, context: NSManagedObjectContext )
+    static var droppedPins = [ Int16 : Pin ]()
+    static var currentPins = [ MKPinAnnotationView : Int16 ]()
+    
+    static var totalPins: Int = 0
+    
+//    static var totalPins: Int16
+//    {
+//        return Int16( self.droppedPins.count )
+//    }
+    
+    init( location: CLLocationCoordinate2D, pin: MKPinAnnotationView, context: NSManagedObjectContext )
     {
-        let coordinateEntity = NSEntityDescription.entityForName(
+        let pinEntity = NSEntityDescription.entityForName(
             "Pin",
             inManagedObjectContext: context
         )!
         
         super.init(
-            entity: coordinateEntity,
+            entity: pinEntity,
             insertIntoManagedObjectContext: context
         )
         
+        Pin.totalPins++
+        
         pinLatitude = location.latitude
         pinLongitude = location.longitude
-        pinNumber = number
+        pinNumber = Int16( Pin.totalPins )
         mapPin = pin
+        println( "initing with mapPin.annotation: \( pin.annotation )." )
+        
+        // Pin.totalPins++
+        
+        Pin.droppedPins.updateValue( self, forKey: pinNumber )
+        Pin.currentPins.updateValue( pinNumber, forKey: mapPin )
     }
     
     override init( entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext? )
@@ -43,5 +61,44 @@ class Pin: NSManagedObject
             entity: entity,
             insertIntoManagedObjectContext: context
         )
+    }
+    
+    class func fetchAllPins()
+    {
+        println( "fetching all pins..." )
+        let fetchError: NSErrorPointer = nil
+        
+        let pinsFetchRequest = NSFetchRequest( entityName: "Pin" )
+        
+        let pins: [ AnyObject ] = CoreDataStackManager.sharedInstance().managedObjectContext!.executeFetchRequest(
+            pinsFetchRequest,
+            error: fetchError
+        )!
+        println( "pins.count: \( pins.count )" )
+        
+        if fetchError != nil
+        {
+            println( "There was an error fetching the pins from Core Data: \( fetchError )." )
+        }
+        
+//        if !pins.isEmpty
+//        {
+//            let lastPin: Pin! = pins.last! as? Pin
+//            totalPins = lastPin.pinNumber
+//        }
+        
+        // println( totalPins )
+        
+        var lastPin: Int = 0
+        for pin in pins
+        {
+            println( "Ading \( pin ) to Pin.droppedPins." )
+            Pin.droppedPins.updateValue( pin as! Pin, forKey: pin.pinNumber )
+            lastPin = Int( pin.pinNumber )
+        }
+        
+        totalPins = lastPin
+        
+        // return pins as! [ Pin ]
     }
 }
