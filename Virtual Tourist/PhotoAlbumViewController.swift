fec3f386d97e921c67c2b53eccce819853e1d9a0
Aaron <J>
2015-08-04 16:45:40 -0700

    Photo Album views are almost all laid out.
    Just missing the collection view.

---
+++ Virtual Tourist/PhotoAlbumViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PhotoAlbumViewController.swift
+//  Virtual Tourist
+//
+//  Created by Aaron Justman on 8/4/15.
+//  Copyright (c) 2015 AaronJ. All rights reserved.
+//
+
+import UIKit
+
+class PhotoAlbumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
